83a8e0d61b56a74f51ae18ac03078cb0821a23c7
Carlos Molina <cfmolinaforero@gmail.com>
2020-08-18 01:59:50 -0400

    Paginate results

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -7,11 +7,32 @@
  * context - shared across all resolvers for particular operation (used for per-operation state sharing)
  * info - info about execution state
  */
+const { paginateResults } = require('./utils');
 
 module.exports = {
    Query: {
-      launches: (_, __, { dataSources }) =>
-         dataSources.launchAPI.getAllLaunches(),
+      // launches: (_, __, { dataSources }) =>
+      //    dataSources.launchAPI.getAllLaunches(),
+      launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+         const allLaunches = await dataSources.launchAPI.getAllLaunches();
+         // we want these in reverse chronological order
+         allLaunches.reverse();
+         const launches = paginateResults({
+            after,
+            pageSize,
+            results: allLaunches,
+         });
+         return {
+            launches,
+            cursor: launches.length
+               ? launches[launches.length - 1].cursor
+               : null,
+            hasMore: launches.length
+               ? launches[launches.length - 1].cursor !==
+                 allLaunches[allLaunches.length - 1].cursor
+               : false,
+         };
+      },
       launch: (_, { id }, { dataSources }) =>
          dataSources.launchAPI.getLaunchById({ launchId: id }),
       me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
